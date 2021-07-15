768f46094da657fd73ac3d1a46e66d809877b0ba
yokra9 <53964890+yokra9@users.noreply.github.com>
2021-07-15 15:05:41 +0000

    resize image to specified size on WASM

--- sample/src/index.ts
+++ sample/src/index.ts
@@ -1,4 +1,50 @@
-const js = import("../node_modules/wasm-image-resizer/wasm_image_resizer");
-js.then(js => {
-  js.greet("WebAssembly");
+import type * as WASM from "wasm-image-resizer"
+type Wasm = typeof WASM;
+
+// WASMのShimを動的インポートする
+const js = import("wasm-image-resizer/wasm_image_resizer");
+js.then(async wasm => {
+  // 画像をWASMインスタンスのメモリ上に格納する
+  const { ptr, len } = await loadImageToMemory('https://raw.githubusercontent.com/rustwasm/wasm-pack/master/demo.gif', wasm);
+
+  const format = "jpg";
+
+  // WASM側でメモリから画像を読み込んでリサイズ処理を行う
+  const base64 = wasm.resize_image_to_base64(ptr, len, 100, 200, format);
+
+  // base64画像を読み込み
+  const e = document.getElementById("sample");
+  //e.setAttribute("src", `data:image/${format};base64,${base64}`);
+
+  const blob = await base64ToBlob(base64, `image/${format}`);
+  e.setAttribute("src", URL.createObjectURL(blob));
+
 });
+
+async function base64ToBlob(base64: string, mime: string) {
+  const resp = await fetch(`data:${mime};base64,${base64}`);
+  return await resp.blob();
+}
+
+async function loadImageToMemory(img: string, { alloc, wasm_memory }: Wasm) {
+
+  // URLから画像を取得してバッファに入れる
+  const resp = await fetch(img);
+  const buf = await resp.arrayBuffer();
+
+  // バッファの長さを確認する
+  const len = buf.byteLength;
+  console.log("len", len);
+
+  // WASMインスタンス側で同サイズのバッファを確保してポインタを取得する
+  const ptr = alloc(len);
+
+  // WASMインスタンスのメモリ上のバイト配列を表示する型付き配列ビューを作成する
+  const memory = wasm_memory() as WebAssembly.Memory;
+  const imgArray = new Uint8Array(memory.buffer, ptr, len);
+
+  // バッファから型付き配列を作成し、WASMインスタンスのメモリ上に格納する
+  imgArray.set(new Uint8Array(buf));
+
+  return { ptr, len };
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,83 @@
+extern crate base64;
+extern crate console_error_panic_hook;
 extern crate wasm_bindgen;
+extern crate web_sys;
 
+use image::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    pub fn log(s: &str);
 }
 
+// WASMインスタンスのアクセスするメモリへのハンドルを返す
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub fn wasm_memory() -> JsValue {
+    wasm_bindgen::memory()
+}
+
+// 指定された長さのバッファを確保してポインタを返す
+#[wasm_bindgen]
+pub fn alloc(len: usize) -> *mut u8 {
+    let mut buf = Vec::with_capacity(len);
+    let ptr = buf.as_mut_ptr();
+    std::mem::forget(buf);
+    ptr
+}
+
+// 画像を指定の縦横幅にリサイズしてbase64で返す
+#[wasm_bindgen]
+pub fn resize_image_to_base64(
+    ptr: *mut u8,
+    len: usize,
+    width: usize,
+    height: usize,
+    fmt: &str,
+) -> String {
+    // バッファから画像を読み込む
+    let img = load_from_buffer(ptr, len);
+
+    // 指定サイズに画像をリサイズする
+    let resized = img.resize(width as u32, height as u32, imageops::FilterType::Triangle);
+    // バッファに画像を書き出す
+    let result = save_to_buffer(resized, fmt);
+
+    // バッファ上の画像をbase64に変換
+    let base64 = base64::encode(&result);
+    base64
+}
+
+// バッファから画像を読み込む
+fn load_from_buffer(ptr: *mut u8, len: usize) -> DynamicImage {
+    console_error_panic_hook::set_once();
+    // 指定されたポインタとバイト長からバッファを取得
+    let src = unsafe { Vec::from_raw_parts(ptr, len, len) };
+    let img = load_from_memory(&src).expect("Error occurs at load image from buffer.");
+
+    img
+}
+
+// バッファに画像を書き出す
+fn save_to_buffer(img: DynamicImage, fmt_str: &str) -> Vec<u8> {
+    console_error_panic_hook::set_once();
+
+    let fmt = match fmt_str {
+        "png" => ImageOutputFormat::Png,
+        "gif" => ImageOutputFormat::Gif,
+        "bmp" => ImageOutputFormat::Bmp,
+        "tga" => ImageOutputFormat::Tga,
+        "jpg" => ImageOutputFormat::Jpeg(80),
+        unsupport => ImageOutputFormat::Unsupported(String::from(unsupport)),
+    };
+
+    // バッファを確保して画像を書き出す
+    let mut result: Vec<u8> = Vec::new();
+    img.write_to(&mut result, fmt)
+        .expect("Error occurs at save image from buffer.");
+
+    log(&format!("result {}", result.len()));
+
+    result
 }
