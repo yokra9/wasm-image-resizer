dcc384442ca54cf327f9c70695bee2f02493c7e9
yokra9 <53964890+yokra9@users.noreply.github.com>
2021-07-20 14:46:50 +0000

    compare with WebGL

--- src/lib.rs
+++ src/lib.rs
@@ -53,11 +53,11 @@ pub fn resize_image(ptr: *mut u8, len: usize, width: usize, height: usize, fmt: 
     timeEnd("load_from_buffer");
     
     // 指定サイズに画像をリサイズする
-    time("thumbnail");
-    let resized = img.thumbnail(width as u32, height as u32);
+    time("resize_to_fill");
+    let resized = img.resize_to_fill(width as u32, height as u32, imageops::FilterType::Triangle);
     // バッファに画像を書き出す
     let mut result = save_to_buffer(resized, fmt);
-    timeEnd("thumbnail");
+    timeEnd("resize_to_fill");
     
     time("BufferValue");
     // バッファのポインタとデータ長をJSに返却する
